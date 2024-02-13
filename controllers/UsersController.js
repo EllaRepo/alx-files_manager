fc94c63029e0319f96631cd4e98d74b9427d69be
Eliab E <eliaberango@gmail.com>
2024-02-13 09:59:31 -0800

    Add controllers/UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,27 +4,31 @@ import dbClient from '../utils/db';
 class UsersController {
   static postNew(req, res) {
     const { email, password } = req.body;
-
     if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+      res.status(400).json({ error: 'Missing email' });
+      return;
     }
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      res.status(400).json({ error: 'Missing password' });
+      return;
     }
 
-    const user = dbClient.db.collection('users');
-    user.findOne({ email })
-      .then((user) => {
-        if (user) {
-          return res.status(400).json({ error: 'Already exist' });
-        }
-        const hashedPwd = sha1(password);
-        return user.insertOne({ email, password: hashedPwd });
-      })
-      .then((result) => {
-        res.status(201).json({ id: result.insertedId, email });
-      })
-      .catch((err) => console.log(err));
+    const users = dbClient.db.collection('users');
+    users.findOne({ email }, (err, user) => {
+      if (user) {
+        res.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = sha1(password);
+        users.insertOne(
+          {
+            email,
+            password: hashedPassword,
+          },
+        ).then((result) => {
+          res.status(201).json({ id: result.insertedId, email });
+        }).catch((error) => console.log(error));
+      }
+    });
   }
 }
 
