ca608aa1989a46f81486c89410cc733d6c597ec0
Eliab E <eliaberango@gmail.com>
2024-02-13 09:34:20 -0800

    Fix style error

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,10 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
-const UsersController = {
-  postNew: async (req, res) => {
+class UsersController {
+  static postNew(req, res) {
     const { email, password } = req.body;
+
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
@@ -12,23 +13,19 @@ const UsersController = {
     }
 
     const user = dbClient.db.collection('users');
-    user.findOne({ email }, (err, usr) => {
-      if (usr) {
-        res.status(400).json({ error: 'Already exist' });
-      } else {
-        const hashedpwd = sha1(password);
-        user.insertOne(
-          {
-            email,
-            password: hashedpwd,
-          },
-        ).then((result) => {
-          res.status(201).json({ id: result.insertedId, email });
-        }).catch((err) => console.log(err));
-      }
-    });
-  },
-
-};
+    user.findOne({ email })
+      .then((user) => {
+        if (user) {
+          return res.status(400).json({ error: 'Already exist' });
+        }
+        const hashedPwd = sha1(password);
+        return user.insertOne({ email, password: hashedPwd });
+      })
+      .then((result) => {
+        res.status(201).json({ id: result.insertedId, email });
+      })
+      .catch((err) => console.log(err));
+  }
+}
 
 module.exports = UsersController;
