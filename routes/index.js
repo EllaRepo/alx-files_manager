2f1c5a6d3d6963ab7f280c409bb1b252ae55bf23
Eliab E <eliaberango@gmail.com>
2024-02-13 23:35:54 -0800

    Removed duplicated route

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ const UsersController = require('../controllers/UsersController');
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
