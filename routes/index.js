8e2b3c477d4edd97fe4572ab68b9381312100747
Eliab E <eliaberango@gmail.com>
2024-02-13 23:51:42 -0800

    Rearrange routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const express = require('express');
-
-const router = express.Router();
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
+const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
