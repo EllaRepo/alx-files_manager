1ffb2cc316d9ece4ddae0ad304a9840d8dff59a2
Eliab E <eliaberango@gmail.com>
2024-02-13 23:46:35 -0800

    refactored the code

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,22 +7,23 @@ import redisClient from '../utils/redis';
 const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
 
 class UsersController {
-  static postNew(req, res) {
-    const { email } = req.body;
-    const { password } = req.body;
+  static postNew(request, response) {
+    const { email } = request.body;
+    const { password } = request.body;
+
     if (!email) {
-      res.status(400).json({ error: 'Missing email' });
+      response.status(400).json({ error: 'Missing email' });
       return;
     }
     if (!password) {
-      res.status(400).json({ error: 'Missing password' });
+      response.status(400).json({ error: 'Missing password' });
       return;
     }
 
     const users = dbClient.db.collection('users');
     users.findOne({ email }, (err, user) => {
       if (user) {
-        res.status(400).json({ error: 'Already exist' });
+        response.status(400).json({ error: 'Already exist' });
       } else {
         const hashedPassword = sha1(password);
         users.insertOne(
@@ -31,15 +32,15 @@ class UsersController {
             password: hashedPassword,
           },
         ).then((result) => {
-          res.status(201).json({ id: result.insertedId, email });
+          response.status(201).json({ id: result.insertedId, email });
           userQueue.add({ userId: result.insertedId });
         }).catch((error) => console.log(error));
       }
     });
   }
 
-  static async getMe(req, res) {
-    const token = req.header('X-Token');
+  static async getMe(request, response) {
+    const token = request.header('X-Token');
     const key = `auth_${token}`;
     const userId = await redisClient.get(key);
     if (userId) {
@@ -47,13 +48,13 @@ class UsersController {
       const idObject = new ObjectID(userId);
       users.findOne({ _id: idObject }, (err, user) => {
         if (user) {
-          res.status(200).json({ id: userId, email: user.email });
+          response.status(200).json({ id: userId, email: user.email });
         } else {
-          res.status(401).json({ error: 'Unauthorized' });
+          response.status(401).json({ error: 'Unauthorized' });
         }
       });
     } else {
-      res.status(401).json({ error: 'Unauthorized' });
+      response.status(401).json({ error: 'Unauthorized' });
     }
   }
 }
